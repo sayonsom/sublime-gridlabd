3a7fd9175c44e47f7ec66060cb90b8b22dd1fada
sayonsom <sayon@ieee.org>
2019-08-20 21:45:39 -0400

    added new keywords

--- extract_keywords/main.py
+++ extract_keywords/main.py
@@ -1,13 +1,34 @@
 from bs4 import BeautifulSoup
-
+import requests
 
 # Save the HTML page of interest, and pass the HTML File address here
-url = "C:\\Users\\CHANDS3\\Desktop\\Power Flow User Guide - GridLAB-D Wiki.html"
-soup = BeautifulSoup(url.read())
 
 
-xx = soup.find_all("pre")
 
-print(xx)
+r  = requests.get("http://gridlab-d.shoutwiki.com/wiki/Power_Flow_User_Guide")
+data = r.text
 
-lore
+soup = BeautifulSoup(data, features="lxml")
+
+
+tables = soup.findAll("table")
+syntax_list = []
+
+for t in tables:
+    count = 0
+    for row in t.tbody.findAll('tr'):
+        if count == 0:
+            count += 1
+            continue
+
+        else:
+            x = str(row.find('td'))
+            x = x.replace("<td>","").replace("\n</td>","").replace(",","").replace("<td align=\"center\">","")
+            syntax_list.append(x)
+    
+syntax_string = ""
+
+for s in syntax_list:
+    syntax_string += s + "|"
+
+print(syntax_string)
